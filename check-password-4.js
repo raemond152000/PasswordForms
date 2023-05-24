539988a945b51c237c55f5e9e5f8fdf604cc73ba
raemond152000 <44762665+raemond152000@users.noreply.github.com>
2023-05-24 15:49:48 -0400

    Update check-password-4.js

--- check-password-4.js
+++ check-password-4.js
@@ -18,24 +18,33 @@ form.addEventListener(
   "focus",
   function (event) {
     event.target.style.background = "lightblue";
+    
   },
 
   true
 );
 
 
+
 function passwordMask() {
+  
+  var ele = document.getElementById("pswrd");
+  var str = ele.value;
   let text = document.getElementById("pswrd").value;
-  /* document.getElementById("demo").innerHTML = "You wrote: " + text; */
+  /* ele.setAttribute('data-orig', str);
+  
+  
   let reg = /.{1,7}/;
   let emptyStr = "";
   let userInput = text;
-   /* myPass.store = text; */
+  
+  
   emptyStr = userInput;
 
   document.getElementById("pswrd").value = emptyStr.replace(reg, (m) =>
     "*".repeat(m.length)
-  );
+  ); */
+  
 }
 
 function toggleVisibility() {
@@ -48,19 +57,110 @@ function toggleVisibility() {
 }
 
 function mouseoverPass() {
-  let obj = document.getElementById("pswrd");
-  obj.type = "text";
-  document.getElementById("demo").innerText = obj.value;
-  alert(myPassword.store)
+ 
+ 
+  
+  /* var ele = document.getElementById("pswrd");
+  var str = ele.getAttribute('data-orig');
+  ele.value = str;
+  document.getElementById("demo").innerText = str; */
+
+  /* let text = document.getElementById("pswrd").value;
+  
+  
+  
+  let reg = /.{1,7}/;
+  let emptyStr = "";
+  let userInput = text;
+  
+  
+  emptyStr = userInput;
+
+  document.getElementById("pswrd").value = emptyStr.replace(reg, (m) =>
+    "*".repeat(m.length)) */
+
 }
 
 function mouseoutPass() {
-  let obj = document.getElementById("pswrd");
-  obj.type = "text";
+  let text = document.getElementById("pswrd").value;
   
+  
+  
+  let reg = /.{1,7}/;
+  let emptyStr = "";
+  let userInput = text;
+  
+  
+  emptyStr = userInput;
+
+  document.getElementById("pswrd").value = emptyStr.replace(reg, (m) =>
+    "*".repeat(m.length))
+  
+  
+}
+
+function myReplace() {
+  var ele = document.getElementById("test");
+  var str = ele.innerHTML;
+  ele.setAttribute('data-orig', str);
+  var res = str.replace("O", "!").replace("E", "#")
+    .replace("T", "%").replace("N", "&");
+  ele.innerHTML = res;
+}
+
+function myRestore() {
+  var ele = document.getElementById("test");
+  var str = ele.getAttribute('data-orig');
+  ele.innerHTML = str;
 }
 
 /* let reg = /.{1,5}/
 let string = '123456789';
 let string2 = '123';
 console.log(string.replace(reg, (m) => "X".repeat(m.length))); */
+function createstars(n) {
+  return new Array(n+1).join("*")
+}
+
+
+/* $(document).ready(function() {
+
+    var timer = "";
+  
+    $(".panel").append($('<input type="text" class="hidpassw" />'));
+  
+    $(".hidpassw").attr("name", $(".pass").attr("name"));
+  
+    $(".pass").attr("type", "text").removeAttr("name");
+  
+    $("body").on("keypress", ".pass", function(e) {
+      var code = e.which;
+      if (code >= 32 && code <= 127) {
+        var character = String.fromCharCode(code);
+        $(".hidpassw").val($(".hidpassw").val() + character);
+      }
+  
+  
+    });
+  
+    $("body").on("keyup", ".pass", function(e) {
+      var code = e.which;
+  
+      if (code == 8) {
+        var length = $(".pass").val().length;
+        $(".hidpassw").val($(".hidpassw").val().substring(0, length));
+      } else if (code == 37) {
+  
+      } else {
+        var current_val = $('.pass').val().length;
+        $(".pass").val(createstars(current_val - 1) + $(".pass").val().substring(current_val - 1));
+      }
+  
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        $(".pass").val(createstars($(".pass").val().length));
+      }, 200);
+  
+    });
+  
+  }); */
